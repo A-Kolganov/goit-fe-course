f163b00ed2bc42149825431f0af7b28694b15b1a
A-Kolganov <tema.crow@gmail.com>
2018-11-16 13:09:23 +0200

    remake

--- js/module_03/js/index.js
+++ js/module_03/js/index.js
@@ -2,35 +2,65 @@
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const isLoginValid = function(login) {
-    if (login.length >= 4 && login.length <= 16){
+const isLoginValid = (login) => {
+    return login.length >= 4 && login.length <= 16
+}
+const isLoginUnique = (logins, login) => {
+    if (logins.includes(login)){
+        return false;
+    } {
         return true;
+    }
+}
+
+const addLogin = (logins,login) => {
+    if (login !== null) {
+        if (isLoginValid (login) === true){
+            if (isLoginUnique (logins, login) === true){
+                logins.push (login);  
+                alert('Логин успешно добавлен!');
+            } else {
+                alert ('Такой логин уже используется!');
+            }
+        } else {
+            alert ('Ошибка! Логин должен быть от 4 до 16 символов')
+        }
     } else {
-        alert ('Ошибка! Логин должен быть от 4 до 16 символов');
+        alert('Отменено пользователем!');
     }
-};
-
-const isLoginUnique = function(logins, login) {
-    for ( a of logins) {
-        if ( a === login) {
-            return true
-        } 
-    }
-};
-
-const addLogin = function(login) {
-    if (isLoginValid (login) === true && isLoginUnique (logins, login) !== true){
-       logins.push (login);  
-       alert('Логин успешно добавлен!');
-    } else if (isLoginUnique (logins, login) === true){
-        alert ('Такой логин уже используется!');
-    }
-
-};
+}
 
 const login = prompt('Введите новый логин (от 4 до 16-ти символов)');
-if (login === null) {
-    alert('Отменено пользователем!');
-} else {
-addLogin(login);
-}
+addLogin(logins,login);
+
+
+
+
+// const isLoginValid = function(login) {
+//     if (login.length >= 4 && login.length <= 16){
+//         return true;
+//     } else {
+//         alert ('Ошибка! Логин должен быть от 4 до 16 символов');
+//     }
+// };
+
+// const isLoginUnique = function(logins, login) {
+//     return logins.includes(login);
+// };
+
+// const addLogin = function(login) {
+//     if (isLoginValid (login) === true && isLoginUnique (logins, login) !== true){
+//        logins.push (login);  
+//        alert('Логин успешно добавлен!');
+//     } else if (isLoginUnique (logins, login) === true){
+//         alert ('Такой логин уже используется!');
+//     }
+
+// };
+
+// const login = prompt('Введите новый логин (от 4 до 16-ти символов)');
+// if (login === null) {
+//     alert('Отменено пользователем!');
+// } else {
+// addLogin(login);
+// }
