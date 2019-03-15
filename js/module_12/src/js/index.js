d78f80e3055f796331c42613f17815c669ad5371
A-Kolganov <tema.crow@gmail.com>
2019-03-15 11:07:00 +0200

    add reset input, delete btn

--- js/module_12/src/js/index.js
+++ js/module_12/src/js/index.js
@@ -9,16 +9,15 @@ const addLinkInput = document.querySelector('.js-fav-links-inp');
 const addLinkBtn = document.querySelector('.js-fav-links-btn');
 const favLinksList = document.querySelector('.js-fav-links-list');
 
-console.log(linkTpl)
+
 addLinkForm.addEventListener('submit', addLink);
+
 
 function addLink(e){
     e.preventDefault();
     const value = addLinkInput.value;
-    console.log(value)
-    const io = fetchAPI(value);
-    console.log(io)
-
+    fetchAPI(value);
+    addLinkForm.reset();
 }
 function addToList(data){
     const urlData = {
@@ -27,20 +26,20 @@ function addToList(data){
         img : data.image,
         url: data.url
     }
- 
     favLinksList.insertAdjacentHTML("afterbegin", linkTpl(urlData));
+    const deleteLinkBtn = document.querySelector('.js-delete-link-btn');
+    deleteLinkBtn.addEventListener('click', removeFromList)
 }
-function addToLocalStorage(){
-
+function removeFromList(e){
+    e.preventDefault();
+    e.target.removeEventListener('click', removeFromList)
+    this.parentNode.remove(".js-fav-link-item");
 }
 function fetchAPI (url){
     const keyAPI = '5c8a795154b726205b11554ae09686d1186cd15744a02';
     return fetch(`http://api.linkpreview.net/?key=${keyAPI}&q=${url}`).then(response=>{
         if (response.ok) return response.json();
         throw new Error(`Error while fetching: ${response.statusText}`);
-}).then(data=> {  
-    console.log(data)
-    addToList(data);
-  }
+}).then(data=> {addToList(data)}
 ).catch(err=>console.log(err));
 }
